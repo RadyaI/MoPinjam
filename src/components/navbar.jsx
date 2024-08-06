47ee493015d5ad355b8d11dfcf8fb78d523ad174
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-08-06 18:57:11 +0700

    First Commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components"
+
+const NavWrapper = styled.div`
+    position:fixed;
+    height:80px;
+    width:100%;
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    z-index: 2;
+    `
+
+const Title = styled.h1`
+    margin-left:40px;
+    color:#222831;
+    `
+const Menu = styled.p`
+    cursor:pointer;
+    `
+
+const Button = styled.button`
+    border:none;
+    padding:10px 30px;
+    border-radius:5px;
+    font-weight:bold;
+    background-color:#222831;
+    color:white;
+    cursor:pointer;
+    `
+
+const Group = styled.div`
+    margin-right:30px;
+    width:220px;
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+    `
+
+export default function Navbar() {
+    return (
+        <>
+            <NavWrapper>
+                <Title>DigiPerpus</Title>
+                <Group>
+                    <Menu>Pinjam</Menu>
+                    <Button>LOGIN</Button>
+                </Group>
+            </NavWrapper>
+        </>
+    )
+}
---
+++ src/dashboard.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './assets/dashboard.css'
+import Navbar from './components/navbar'
+
+
+export default function Dashboard() {
+
+  const [search, setsearch] = useState(false)
+  const [input, setInput] = useState(null)
+  const [placeholder, setPlaceholder] = useState('Cari buku yang kamu inginkan...')
+
+  function animateCard(e) {
+    if (e.key === "Enter" && input) {
+      setsearch(true)
+      setPlaceholder('Cari buku yang kamu inginkan...')
+    }
+  }
+
+  function searchBook() {
+    if (input === null || input === '') {
+      console.log('belum mencari')
+      setPlaceholder('Harap diisi...')
+    } else {
+      console.log('mencari')
+      setPlaceholder('Cari buku yang kamu inginkan...')
+      setsearch(true)
+    }
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className={`home ${search ? 'home-animate' : ''}`}>
+        <div className={`card ${search ? 'card-animate' : ''}`}>
+          <div className="search">
+            <input type="text" placeholder={placeholder} onKeyUp={(e) => animateCard(e)} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={searchBook}>CARI</button>
+          </div>
+        </div>
+      </div>
+      {search === true && (
+        <div className="data" id='book'>
+
+        </div>
+      )}
+    </>
+  )
+}
