e0faefabf803c09b00a942e27e8a412e96f2dcf0
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-08-06 20:25:52 +0700

    add: meta deskripsi

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import router from './router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <meta name="description" content="Sistem peminjaman buku online untuk perpustakaan" />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
