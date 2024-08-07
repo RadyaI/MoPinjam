16dbc6a7b94cc0537cf70fd40621e6a5ca88e9df
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-08-07 18:44:43 +0700

    add: desain buat card buku

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -8,11 +8,13 @@ export default function Dashboard() {
   const [search, setsearch] = useState(false)
   const [input, setInput] = useState(null)
   const [placeholder, setPlaceholder] = useState('Cari buku yang kamu inginkan...')
+  const [sambutan, setSambutan] = useState('Selamat Datang di DigiPerpus')
 
   function animateCard(e) {
     if (e.key === "Enter" && input) {
       setsearch(true)
       setPlaceholder('Cari buku yang kamu inginkan...')
+      setSambutan('')
     }
   }
 
@@ -24,6 +26,7 @@ export default function Dashboard() {
       console.log('mencari')
       setPlaceholder('Cari buku yang kamu inginkan...')
       setsearch(true)
+      setSambutan('')
     }
   }
 
@@ -32,7 +35,7 @@ export default function Dashboard() {
       <Navbar />
       <div className={`home ${search ? 'home-animate' : ''}`}>
         <div className={`card ${search ? 'card-animate' : ''}`}>
-          <h2>Selamat Datang di DigiPerpus</h2>
+          <h2>{sambutan}</h2>
           <div className="search">
             <input type="text" placeholder={placeholder} onKeyUp={(e) => animateCard(e)} onChange={(e) => setInput(e.target.value)} />
             <button onClick={searchBook}>CARI</button>
@@ -41,7 +44,14 @@ export default function Dashboard() {
       </div>
       {search === true && (
         <div className="data" id='book'>
+          <div className="card-container">
 
+            <div className="card">
+              <div className="img-cover"><img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" alt="Buku" /></div>
+              <div className="desc"></div>
+            </div>
+
+          </div>
         </div>
       )}
     </>
