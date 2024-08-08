401e0595cb4c5a8672de22df7f295e0a40992e7b
RadyaI <radyaiftikhar@gmail.com>
2024-08-08 20:11:30 +0700

    update: sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,58 @@
+import { useState } from 'react'
+import '../assets/sidebar.css'
+
 export default function Sidebar() {
+
+    const [selectedOption, setSelectedOption] = useState('semua')
+    const [basicFilter, setBasicFilter] = useState('terbaru')
+
     return (
-        <></>
+        <>
+            <div className="sidebar">
+                <div className="card">
+                    <div className="filter">
+                        <h2>Filter</h2>
+                        <div className="basic-filter">
+                            <select value={basicFilter} onChange={(e) => setBasicFilter(e.target.value)}>
+                                <option value="terbaru">Terbaru</option>
+                                <option value="tersedia">Tersedia</option>
+                            </select>
+                        </div>
+                        <div className="stok">
+                            <p>Filter berdasarkan stok</p>
+                            <div className="input">
+                                <div className="tersedia">
+                                    <input
+                                        type="radio"
+                                        value="tersedia"
+                                        checked={selectedOption === 'tersedia'}
+                                        onChange={(e) => setSelectedOption(e.target.value)}
+                                    />
+                                    <label>Tersedia</label>
+                                </div>
+                                <div className="semua">
+                                    <input
+                                        type="radio"
+                                        value="semua"
+                                        checked={selectedOption === 'semua'}
+                                        onChange={(e) => setSelectedOption(e.target.value)}
+                                    />
+                                    <label>Semua</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="kategori">
+                            <p>Kategori</p>
+                            <ul>
+                                <li>Buku Sekolah</li>
+                                <li>Teknologi</li>
+                                <li>Makanan/Minuman</li>
+                                <li>Komik</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
--- src/semuaBuku.jsx
+++ src/semuaBuku.jsx
@@ -1,11 +1,13 @@
 import './assets/semuabuku.css'
 import Navbar from './components/navbar'
+import Sidebar from './components/sidebar'
 
 export default function Semuabuku() {
     return (
         <>
             <Navbar></Navbar>
             <div className="buku-container">
+                <Sidebar></Sidebar>
                 <div className="content"></div>
             </div>
         </>
