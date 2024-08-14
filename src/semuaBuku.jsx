af29c1d58190eca3ad0ec31df24fcb2c9b5093d6
RadyaI <radyaiftikhar@gmail.com>
2024-08-14 20:31:23 +0700

    add (semua buku): tambah buku section untuk display semua buku

--- src/semuaBuku.jsx
+++ src/semuaBuku.jsx
@@ -8,6 +8,25 @@ export default function Semuabuku() {
     const [search, setSearch] = useState(null)
     const [filter, setFilter] = useState('Terbaru')
 
+    const book = [
+        { author: 'Fulan', title: 'How to basic' },
+        { author: 'Fulan', title: 'How to basic' },
+    ]
+
+    function BookList() {
+        const data = book.map(i =>
+            <div className="card">
+                <div className="img-cover"><img loading='lazy' src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" alt="Buku" /></div>
+                <div className="desc">
+                    <div className="author"><small>{i.author}</small></div>
+                    <div className="title">{i.title}</div>
+                </div>
+            </div>
+        )
+
+        return data
+    }
+
     return (
         <>
             <Navbar></Navbar>
@@ -20,6 +39,19 @@ export default function Semuabuku() {
                             <option value="terbaru">Terbaru</option>
                             <option value="tersedia">Tersedia</option>
                         </select>
+                    </div>
+                    <div className="buku-data">
+
+                        {/* <div className="card">
+                            <div className="img-cover"><img loading='lazy' src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" alt="Buku" /></div>
+                            <div className="desc">
+                                <div className="author"><small>My Name Here</small></div>
+                                <div className="title">Secrets in a silicon valley startup</div>
+                            </div>
+                        </div> */}
+
+                        <BookList></BookList>
+
                     </div>
                 </div>
             </div>
