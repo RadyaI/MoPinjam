4d8f700146f874e0af58c15c00250c59a4dccac1
RadyaI <radyaiftikhar@gmail.com>
2024-08-15 15:42:10 +0700

    add: config firebase

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE,
+    authDomain: "mopinjam-2aa4b.firebaseapp.com",
+    projectId: "mopinjam-2aa4b",
+    storageBucket: "mopinjam-2aa4b.appspot.com",
+    messagingSenderId: "201549422692",
+    appId: "1:201549422692:web:7ea2929a37094daf4fff71",
+    measurementId: "G-HB392SSZXV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth }
